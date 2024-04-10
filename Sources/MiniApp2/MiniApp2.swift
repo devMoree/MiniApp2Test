d0da2861e110d14f8eed08a13e39ef876ced25f2
gun <g.gun007@hotmail.com>
2024-04-10 09:11:36 +0700

    new app

---
+++ Sources/MiniApp2/MiniApp2.swift
@@ -0,0 +1,16 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+import Foundation
+import SwiftUI
+
+@available(iOS 13.0, *)
+public struct DemoScreenView: View {
+    
+    @ViewBuilder public var body: some View {
+        ZStack {
+            VStack {
+                Text("Hi , my name is mini app 2 ")
+            }
+        }
+    }
+}
---
+++ Tests/MiniApp2Tests/MiniApp2Tests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import MiniApp2
+
+final class MiniApp2Tests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
